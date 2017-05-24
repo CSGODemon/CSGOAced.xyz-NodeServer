74a7959cae60e968bca925e958ad5570e00cc7a9
TiagoSeverino <TiagoSeverino@users.noreply.github.com>
2017-05-24 09:42:16 +0100

    Add Both User Avatar to Bet

--- index.js
+++ index.js
@@ -2,12 +2,12 @@ var io = require('socket.io').listen(3000);
 
 var bets = [];
 
-var Bet = function(name1, name2, avatar1, avatar2, ammount){
+var Bet = function(name, avatar, ammount){
 	this.id = bets.length;
-	this.name1 = name1;
-	this.name2 = name2;
-	this.avatar1 = avatar1;
-	this.avatar2 = avatar2;
+	this.name1 = name;
+	this.name2 = "";
+	this.avatar1 = avatar;
+	this.avatar2 = "";
 	this.ammount = ammount;
 	this.winner = Math.floor(Math.random()*2);
 	this.isFinished = false;
@@ -19,8 +19,8 @@ var ammout = 900;
 var avatar1 = "https://www.csgoaced.xyz/img/avatar1.jpg";
 var avatar2 = "https://www.csgoaced.xyz/img/avatar2.jpg";
 
-bets.push(new Bet(name1, name2, avatar1, avatar2, ammout));
-bets.push(new Bet(name2, name1, avatar2, avatar1, ammout));
+bets.push(new Bet(name1, avatar1, ammout));
+bets.push(new Bet(name2, avatar2, ammout));
 
 io.on('connection', function(socket){
 	socket.emit('show place bet');
@@ -33,17 +33,22 @@ io.on('connection', function(socket){
 	});
 
 	socket.on('place bet', function(User){
-		var bet = new Bet(User.name, name2, User.avatar, avatar2, ammout);
+		var bet = new Bet(User.name, User.avatar, ammout);
 		bets.push(bet);
 		io.emit('display bet', bet);
 	});
 
-	socket.on('join bet', function(BetID){
+	socket.on('join bet', function(MyBet){
 		bets.forEach(function(bet){
 			if (!bet.isFinished){
-				if (bet.id == BetID){
-					io.emit('flip', bet);
+				if (bet.id == MyBet.id){
+
+					bet.avatar2 = MyBet.avatar;
+					bet.name2 = MyBet.name;
+					bet.ammount *= 2;
 					bet.isFinished =  true;
+
+					io.emit('flip', bet);
 				}
 			}
 		});
